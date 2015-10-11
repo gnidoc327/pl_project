f9faa4ba834e22c38e1b33b4d58493efed83aeb9
kimhs327 <kimhs327@hanmail.net>
2015-10-11 21:36:17 +0900

    parser modify for command example save_formula a 123

--- PL/parser.cpp
+++ PL/parser.cpp
@@ -15,54 +15,63 @@ std::vector<std::string> StringParser::parsing(){
 
 	//check command
 	if (temp_line.at(0) != '(') {
-		result.push_back(this->data);
-		return result;
-	}
-
-	//calc part no command
-	temp_line.erase(temp_line.begin());
-	temp_line.erase(temp_line.end() - 1);
-	pos = temp_line.find(' ') + 1;
-		
-	//check inner calc
-	while (pos < temp_line.length()) {
-		if (temp_line.at(pos) == '(') {
-			//calc
-			pos = temp_line.find(' ');
+	//command
+		int command_max=0;
+		while ((pos = temp_line.find(' ')) != std::string::npos) {
 			temp_string = temp_line.substr(0, pos);
-			temp_line.erase(0, pos+1);
 			result.push_back(temp_string);
+			temp_line.erase(0, pos + 1);
+			command_max++;
+			if (command_max > 1) break;
+		}
+		result.push_back(temp_line);
+		return result;
+	} else {
+	//calc part, no command
+		temp_line.erase(temp_line.begin());
+		temp_line.erase(temp_line.end() - 1);
 
-			//parameter1
-			pos = 0;
-			countParenthesis = 0;
-			while (pos < temp_line.length()) {
-				if (temp_line.at(pos) == '(') countParenthesis++;
-				if (temp_line.at(pos) == ')') countParenthesis--;
-				if (countParenthesis == 0) break;
-				pos++;
-			}
+		pos = temp_line.find(' ') + 1;
 
-			if (pos == 0) {
+		//check inner calc
+		while (pos < temp_line.length()) {
+			if (temp_line.at(pos) == '(') {
+				//calc
 				pos = temp_line.find(' ');
 				temp_string = temp_line.substr(0, pos);
-				temp_line.erase(0, pos+1);
-			} else {
-				temp_string = temp_line.substr(0, pos+1);
-				temp_line.erase(0, pos+2);
-			}
-			result.push_back(temp_string);
+				temp_line.erase(0, pos + 1);
+				result.push_back(temp_string);
 
-			//parameter2
-			if (!temp_line.empty()) {
-				result.push_back(temp_line);
+				//parameter1
+				pos = 0;
+				countParenthesis = 0;
+				while (pos < temp_line.length()) {
+					if (temp_line.at(pos) == '(') countParenthesis++;
+					if (temp_line.at(pos) == ')') countParenthesis--;
+					if (countParenthesis == 0) break;
+					pos++;
+				}
+
+				if (pos == 0) {
+					pos = temp_line.find(' ');
+					temp_string = temp_line.substr(0, pos);
+					temp_line.erase(0, pos + 1);
+				}
+				else {
+					temp_string = temp_line.substr(0, pos + 1);
+					temp_line.erase(0, pos + 2);
+				}
+				result.push_back(temp_string);
+
+				//parameter2
+				if (!temp_line.empty()) {
+					result.push_back(temp_line);
+				}
+
+				return result;
 			}
-			
-			for (int i = 0; i < result.size(); i++)
-				std::cout << result.at(i) << std::endl;
-			return result;
-		} 
-		pos++;
+			pos++;
+		}
 	}
 
 	//not exist inner calc
@@ -73,9 +82,6 @@ std::vector<std::string> StringParser::parsing(){
 		temp_line.erase(0, pos + 1);
 	}
 	result.push_back(temp_line);
-
-	for (int i = 0; i < result.size(); i++)
-		std::cout << result.at(i) << std::endl;
 
 	return result;
 }
